457ef2b8937ecb77e0d5004a5a0d205647bee307
appxzm <117379137+appxzm@users.noreply.github.com>
2024-08-01 03:17:25 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,16 +2,13 @@ export default {
   async fetch(request, env) {
     const day1 = 'github.com'
     const day2 = 'raw.githubusercontent.com'
-    const day3 = 'hostloc.com'
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
       let day = new Date()
-      if (day.getDay() % 3 === 0) {
+      if (day.getDay() % 2) {
         url.hostname = day1
-      } else if (day.getDay() % 3 === 1) {
-        url.hostname = day2
       } else {
-        url.hostname = day3
+        url.hostname = day2
       }
       let new_request = new Request(url, request);
       return fetch(new_request);
