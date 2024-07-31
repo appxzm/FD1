45266de9810f97745c8f64121f07dfb53665ee82
appxzm <117379137+appxzm@users.noreply.github.com>
2024-08-01 03:08:05 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,11 +6,11 @@ export default {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
       let day = new Date()
-      if (day.getDay() % 3) {
+      if (day.getDay() % 3 === 0) {
         url.hostname = day1
-      } else {
+      } else if (day.getDay() % 3 === 1) {
         url.hostname = day2
-      }
+      } else {
         url.hostname = day3
       }
       let new_request = new Request(url, request);
