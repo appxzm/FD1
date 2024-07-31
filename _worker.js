dbe73fe478b77a1a6dc4740c437416cacf2534d8
appxzm <117379137+appxzm@users.noreply.github.com>
2024-08-01 03:27:41 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 export default {
   async fetch(request, env) {
     let url = new URL(request.url);
-    let proxyDomains = ['raw.githubusercontent.com', 'github.com', 'hostloc.com'];
-    if (proxyDomains.some(domain => url.pathname.startsWith('/') && url.hostname === domain)) {
+    if (url.pathname.startsWith('/')) {
+      url.hostname = 'raw.githubusercontent.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
